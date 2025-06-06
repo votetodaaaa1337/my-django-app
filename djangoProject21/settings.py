798c73976ccb3a5691a2d7cdf0d3b8974dbc67c5
votetodaaa <it022542@gmail.com>
2025-06-06 19:08:26 +0900

    Fix static files: whitenoise + settings

--- djangoProject21/settings.py
+++ djangoProject21/settings.py
@@ -125,14 +125,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
