2e78baaf09686fbcb561de1ee24708f35b58e85d
votetodaaa <it022542@gmail.com>
2025-06-06 19:24:25 +0900

    Fix static paths: move css and images into static/

--- djangoProject21/settings.py
+++ djangoProject21/settings.py
@@ -125,8 +125,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
